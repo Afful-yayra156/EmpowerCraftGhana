55d6e9db5d8351c29364425b6e7cf05cc8b970d4
Afful-yayra156 <affulyayra15@gmail.com>
2025-05-01 16:37:42 +0000

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,28 +1,7 @@
-<!-- 
-// $servername = 'empowercraft.mysql.database.azure.com';
-// $username = 'empowercraft1234';
-// $password = 'passWord@123';
-// $dbname = 'empowerhub';
-
-// $conn = mysqli_connect($servername, $username, $password, $dbname) 
-//     or die('Unable to connect');
-
-// if ($conn->connect_error) {
-//     die('Connection failed');
-// }
-
-
-
-
-
- 
-
-
-
- <?php
-$servername = 'empowercraft.mysql.database.azure.com';
-$username = 'empowercraft1234@empowercraft'; // Add '@server-name' to username
-$password = 'passWord@123';
+<?php
+$servername = 'localhost';
+$username = 'empowercraft'; 
+$password = '';
 $dbname = 'empowerhub';
 
 // Create connection
@@ -33,3 +12,7 @@ if (!$conn) {
     die('Connection failed: ' . mysqli_connect_error());
 }
 ?>
+
+
+
+ 
